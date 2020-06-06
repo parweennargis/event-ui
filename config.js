00108362939281ccd711c327b222a6db7baa9f2b
Ayush Mittal <ayush.mittal@outlook.com>
2020-06-06 21:17:23 +0530

    added apiurl

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     // apiUrl: 'https://sheltered-anchorage-98656.herokuapp.com'
-    apiUrl: 'http://localhost:5000'
+    // apiUrl: 'http://localhost:5000'
+    apiUrl: 'http://35.182.221.58:5000'
 };

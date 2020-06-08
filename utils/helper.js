fc7f37746905137574366b08d3a823bc96941aff
Ayush Mittal <ayush.mittal@outlook.com>
2020-06-08 23:21:47 +0530

    virtual event

---
+++ utils/helper.js
@@ -0,0 +1,11 @@
+exports.splitDate = (data) => {
+    return data.reduce((prev, curr, index) => {
+        if (index == 3) prev.push({ ad: true });
+        const startDate = new Date(curr.start_date).toDateString().split(' ');
+        curr.startDay = startDate[2];
+        curr.startMonth = startDate[1];
+        curr.startYear = startDate[3];
+        prev.push(curr);
+        return prev;
+    }, []);
+} 
